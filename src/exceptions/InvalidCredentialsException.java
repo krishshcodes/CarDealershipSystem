b75d4a72794a86daf3e68ca2d63ae46b6d060e63
krishshcodes <krishshharma07@gmail.com>
2025-11-21 21:06:30 +0530

    Add files via upload

---
+++ src/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package CarManagement.exceptions;
+
+public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package CarManagement.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
